ef8e2aa22cb91c69434b408253b7e4c9e0c0624c
notreami <notreami@163.com>
2018-01-07 01:34:55 +0800

    去掉typescript，减少技术栈

--- fewok-ui/config-overrides.js
+++ fewok-ui/config-overrides.js
@@ -1,32 +1,17 @@
-const tsImportPluginFactory = require('ts-import-plugin');
-const {getLoader} = require("react-app-rewired");
+const {injectBabelPlugin} = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 module.exports = function override(config, env) {
-    //antd添加到typescript配置里
-    const tsLoader = getLoader(
-        config.module.rules,
-        rule =>
-            rule.loader &&
-            typeof rule.loader === 'string' &&
-            rule.loader.includes('ts-loader')
-    );
 
-    tsLoader.options = {
-        getCustomTransformers: () => ({
-            before: [tsImportPluginFactory({
-                libraryName: 'antd',
-                libraryDirectory: 'es',
-                style: true,
-            })]
-        })
-    };
+    //antd添加到配置里
+    config = injectBabelPlugin(['import', {libraryName: 'antd', style: true}], config);
 
     //修改antd主题
     config = rewireLess.withLoaderOptions({
         modifyVars: {"@primary-color": "#1DA57A"},
     })(config, env);
+
 
     //根据环境设置
     console.log("ui project environment:" + process.env.NODE_ENV);
