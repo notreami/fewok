520776b9aa3720dc5384a173e9c4108327b1f447
notreami <notreami@163.com>
2018-07-03 10:49:47 +0800

    增加前端依赖

--- fewok-dsl/java/com/fewok/dsl/core/bpm/container/ProcessRule.java
+++ fewok-dsl/java/com/fewok/dsl/core/bpm/container/ProcessRule.java
@@ -19,20 +19,24 @@ public class ProcessRule {
     /**
      * 执行方式（同步、异步、并行）（默认 同步）
      */
+    @Builder.Default
     private InvokeType invokeType = InvokeType.SYNC;
 
     /**
      * 等待之前的流程执行
      */
+    @Builder.Default
     private boolean isWaitBeforeProcessor = false;
 
     /**
      * 重试次数（默认 0 次）
      */
+    @Builder.Default
     private int retryCount = 0;
 
     /**
      * 每个流程执行的超时时间（默认 1 s）
      */
+    @Builder.Default
     private int timeoutMillis = 1000;
 }
